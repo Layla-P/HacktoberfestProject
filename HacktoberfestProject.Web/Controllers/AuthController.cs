7a71cfe6164a1e6538f47051ce183e3c5ca4e435
Layla-p <layla.porter@gmail.com>
2020-09-25 16:41:29 +0100

    initial commit with github oauth in place

---
+++ HacktoberfestProject.Web/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HacktoberfestProject.Web.Controllers
+{
+
+    [Route("[controller]/[action]")]
+    public class AuthController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login(string returnUrl = "/")
+        {
+            return Challenge(new AuthenticationProperties() { RedirectUri = returnUrl });
+        }
+    }
+}
---
+++ HacktoberfestProject.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HacktoberfestProject.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
