9f5ea801e7a464a736307f86e348ac8683cb88e9
CrypticEnigma <30286773+CrypticEngima@users.noreply.github.com>
2020-09-27 13:13:01 +0100

    Disabled the testing code for CosmosDb Emulator have left it in the project as a Guide to usage within the app

--- HacktoberfestProject.Web/Startup.cs
+++ HacktoberfestProject.Web/Startup.cs
@@ -37,7 +37,7 @@ namespace HacktoberfestProject.Web
             services.AddLogging();
 
             
-            CosmosTableTest.RunTableStorageTests(services);
+            //CosmosTableTest.RunTableStorageTests(services);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
