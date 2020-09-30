30bb176fe1e0368f038519c19bdd8c6e15b84c55
CrypticEnigma <30286773+CrypticEngima@users.noreply.github.com>
2020-09-30 20:01:16 +0100

    Fixed a merge conflict

--- HacktoberfestProject.Web/Services/ITableService.cs
+++ HacktoberfestProject.Web/Services/ITableService.cs
@@ -9,13 +9,12 @@ namespace HacktoberfestProject.Web.Services
     public interface ITableService
     {
         Task<ServiceResponse<IEnumerable<Pr>>> GetPrsByUsernameAsync(string username);
-<<<<<<< HEAD
+
         Task<ServiceResponse<Pr>> AddPrByUsernameAsync(string username, string owner, string repositoryName, Pr pr);
-=======
+
 
         Task<ServiceResponse<User>> GetUserByUsernameAsync(string username);
 
-        Task<ServiceResponse<IEnumerable<Pr>>> AddPrByUsernameAsync(string username, Pr pr);
->>>>>>> 2ba122a72ce4925fdff519d646af1cf52495d8de
+
     }
 }
