8643add0cef67e1ce2c93dd066f0bc6f27ecb851
Rick van den Bosch <rickvdbosch@outlook.com>
2020-09-30 08:54:15 +0200

    Added the UserinfoViewModel

---
+++ HacktoberfestProject.Web/Models/UserinfoViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace HacktoberfestProject.Web.Models
+{
+    public class UserinfoViewModel
+    {
+        public string Username { get; set; }
+
+        public List<RepositoryViewModel> Repositories {get;set;}
+    }
+
+    public class RepositoryViewModel
+    {
+        public string Name { get; set; }
+
+        public string Owner { get; set; }
+
+        public List<PullRequestViewModel> PullRequests { get; set; }
+
+    }
+
+    public class PullRequestViewModel
+    {
+        public int Id { get; set; }
+
+        public string Url { get; set; }
+    }
+}
