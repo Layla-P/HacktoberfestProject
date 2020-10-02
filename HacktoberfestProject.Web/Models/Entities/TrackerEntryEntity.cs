dfc463817aee316c98eebabf4f0c386fe3c5c1cc
Rick van den Bosch <rickvdbosch@outlook.com>
2020-10-02 17:35:40 +0200

    Removed unused constant

--- HacktoberfestProject.Web/Models/Entities/TrackerEntryEntity.cs
+++ HacktoberfestProject.Web/Models/Entities/TrackerEntryEntity.cs
@@ -4,8 +4,6 @@ namespace HacktoberfestProject.Web.Models.Entities
 {
     public class TrackerEntryEntity : TableEntity
     {
-        private const string ROWKEY_TEMPLATE = "{0}:{1}:{2}";
-
         [IgnoreProperty]
         public string Username
         {
